8c2fe556234a97ef6599d58517e153de2a1de56a
matuck <matuck@ubuntu>
2013-09-12 14:08:48 -0400

    update documentation and comments.

--- TPBTorFeedHandler.php
+++ TPBTorFeedHandler.php
@@ -1,8 +1,22 @@
 <?php
+/**
+ * This file is part of the TorFeed package.
+ *
+ * (c) Mitch Tuck <matuck@matuck.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace matuck\TorFeed\Handler;
 
 use matuck\TorFeed\Items;
 
+/**
+ * TPB handler for Torfeed
+ * Processes The Pirate Bay torrent feeds.
+ *
+ * @author Mitch Tuck<matuck@matuck.com>
+ */
 class TPBTorFeedHandler
 {
     public function __construct($xml, &$items)
